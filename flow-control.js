53e4cc18a306e3458902e16d34efb09db5fed039
Alicia Arevalo <aliciaarevalo89@yahoo.com>
2019-07-11 04:09:32 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -17,11 +17,9 @@ function ageChecker(age) {
     return "You are a teenager!";
   } else if (age <= 12) {
     return "You are a kid";
-  } else (age >= 19)
-    {return "You are a grownup";}
-  }
-
+  } else {return "You are a grownup";}
 }
+
 
 function ternaryTeenager(age) {
 
